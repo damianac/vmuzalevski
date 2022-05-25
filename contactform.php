b898172170554522ea8ec70c801edb677a6b2be5
vmuzalevski <vmuzalevski@gmail.com>
2022-05-25 08:26:53 +0200

    Promene 5.25

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST['submit'])) {
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailForm = $_POST['mail'];
+$message = $_POST['message'];
+
+$mailto = "vmuzalevski@gmail.com";
+$headers = "Form: ".$mailForm;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+mail($mailto, $subject, $txt, $headers);
+
+}
